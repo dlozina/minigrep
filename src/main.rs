152fd9d8b16d6a42ebf546d9dfd183126f70efc8
Dino Lozina <Dino.Lozina@live.com>
2024-04-29 10:17:23 +0200

    add env variable that ignores case

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,20 @@ use std::process;
 
 use minigrep::Config;
 
+// Commands to test minigrep
+//cargo run -- to poem.txt
+// IGNORE_CASE=1 cargo run -- to poem.txt
+// IGNORE_CASE=1 cargo run -- to poem.txt > output.txt
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem with parsing arguments: {err}");
+        eprintln!("Problem with parsing arguments: {err}");
         process::exit(1)
     });
 
     if let Err(e) = minigrep::run(config){
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1)
     }
 }
