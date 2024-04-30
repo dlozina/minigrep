563cadcc6cde0cdd5e3f726e8f9bf593058cd751
Dino Lozina <Dino.Lozina@live.com>
2024-04-30 09:17:32 +0200

    added iterators

--- src/main.rs
+++ src/main.rs
@@ -8,22 +8,13 @@ use minigrep::Config;
 // IGNORE_CASE=1 cargo run -- to poem.txt
 // IGNORE_CASE=1 cargo run -- to poem.txt > output.txt
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem with parsing arguments: {err}");
         process::exit(1)
     });
 
-    if let Err(e) = minigrep::run(config){
+    if let Err(e) = minigrep::run(config) {
         eprintln!("Application error: {e}");
         process::exit(1)
     }
 }
-
-
-
-
-
-
-
